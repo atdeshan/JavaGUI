dab48e8870f28baabf9235b579b6f2a504872a40
tharindu <tharindudeshan325@gmail.com>
2024-05-25 15:51:33 +0530

    first commit

---
+++ untitled/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    public String name;
+    public String gender;
+
+
+
+    public Student(String name,String gender){
+        this.name = name;
+        this.gender = gender;
+    }
+}
